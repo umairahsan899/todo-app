bc2cefbea969d9b31be7a0cde58967e9778459ea
Umair Ahsan <umairahsan899@gmail.com>
2026-07-03 18:48:15 +0500

    h

--- gui.py
+++ gui.py
@@ -1,8 +1,43 @@
-from function import *
+from tkinter import Listbox
+
+import function
 import PySimpleGUI as sg
 label=sg.Text("Type in a To-Do:")
-input_box=sg.InputText(tooltip="Enter To Do:")
+input_box=sg.InputText(tooltip="Enter To Do:",key="todo")
 add_button=sg.Button("Add")
-window=sg.Window("My To-Do App",layout=[[label],[input_box,add_button]])
-values=window.read()
+list_box=sg.Listbox(values=function.get_todos(),key="todos",
+                    enable_events=True,size=(45,10),)
+edit_button=sg.Button("Edit")
+window=sg.Window(title="My To-Do App",
+                 layout=[[label],[input_box,add_button],[list_box,edit_button]],
+                 font=("Helvetica",20))
+while True:
+    event,values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos=function.get_todos()
+            new_todo=values["todo"]+'\n'
+            todos.append(new_todo)
+            function.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit=values['todos'] [0]
+            new_todo=values['todo']
+
+            todos=function.get_todos()
+            index=todos.index(todo_to_edit)
+            todos[index]=new_todo
+            function.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todos'].update(value=values['todos'])
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
+
+
+
