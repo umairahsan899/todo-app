3c8cf5ab3129eb27ce836901b9620cb864e2ac37
Umair Ahsan <umairahsan899@gmail.com>
2026-07-05 12:31:05 +0500

    completion

--- web.py
+++ web.py
@@ -5,14 +5,22 @@ def add_todo():
     todo=st.session_state["new_todo"]+'\n'
     todos.append(todo)
     function.write_todos(todos)
+def remove_todo():
+    todo=st.session_state["new_todo"]+'\n'
+    todos.remove(todo)
+    function.write_todos(todos)
 st.title("My To-Do App")
 st.subheader("This is my Todos App")
 st.write("This app is use to increase your productivity")
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox=st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input("Enter a todo:",
               placeholder="Add new todo....",
               on_change=add_todo,
               key="new_todo")
-st.session_state
